1c5152905358c276af27ba6f76a9f5cd997488d9
Sadikov Vitaly <vitaly.sadikov1@yandex.ru>
2025-08-20 00:02:25 +0500

    feat: added fullName id to find all

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,6 +19,7 @@ export class UserService {
         number: true,
         fullName: {
           select: {
+            id: true,
             fullName: true,
             branch: {
               select: {
