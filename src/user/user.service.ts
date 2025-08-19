39deb2cffbc5e84698bb8682854a42e949f014aa
Sadikov Vitaly <vitaly.sadikov1@yandex.ru>
2025-08-19 22:05:07 +0500

    fix: fixed find all user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,6 +19,7 @@ export class UserService {
         number: true,
         fullName: {
           select: {
+            fullName: true,
             branch: {
               select: {
                 id: true,
